95ee7f05acc05a86fe04598aa9ce2f7da81292de
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-08 11:47:05 +0000

    Refactor Part 1

--- 1/part1.py
+++ 1/part1.py
@@ -2,7 +2,6 @@ with open("input.txt") as f:
     running_total = 0
     for line in f:
         numeric_characters = [chr for chr in line if chr.isnumeric()]
-        first_and_last_character = numeric_characters[0] + numeric_characters[-1]
-        running_total += int(first_and_last_character)
+        running_total += int(numeric_characters[0] + numeric_characters[-1])
 
 print(running_total)
