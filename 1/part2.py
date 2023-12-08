de02e52559d4e8e5eace764bb0eeb32b1e6f6b1e
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-08 12:08:45 +0000

    Refactor Part 2

--- 1/part2.py
+++ 1/part2.py
@@ -1,57 +1,45 @@
-valid_digits = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9"
-}
+from typing import Literal
 
-valid_digits_as_text = valid_digits.keys()
+def find_digit(line: str, digit_position: Literal['first', 'last']) -> str:
+    valid_digits = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+
+    valid_digits_as_text = valid_digits.keys()
+
+    digit = ''
+    non_numeric_characters = ''
+
+    if digit_position == 'last':
+        line = reversed(line)
+
+    for chr in line:
+
+        if chr.isnumeric():
+            return chr
+
+        if digit_position == 'last':
+            non_numeric_characters = chr + non_numeric_characters
+        else:
+            non_numeric_characters += chr
+
+        for valid_digit in valid_digits_as_text:
+            if valid_digit in non_numeric_characters:
+                return valid_digits[valid_digit]
+
 
 running_total = 0
 
 with open("input.txt") as f:
     for line in f:
-
-        first_digit = ''
-        non_numeric_characters = ''
-
-        for chr in line:
-
-            if chr.isnumeric():
-                first_digit = chr
-
-            non_numeric_characters += chr
-
-            for digit in valid_digits_as_text:
-                if digit in non_numeric_characters:
-                    first_digit = valid_digits[digit]
-
-            if first_digit:
-                break
-
-        last_digit = ''
-        non_numeric_characters = ''
-
-        for chr in reversed(line):
-
-            if chr.isnumeric():
-                last_digit = chr
-
-            non_numeric_characters = chr + non_numeric_characters
-
-            for digit in valid_digits_as_text:
-                if digit in non_numeric_characters:
-                    last_digit = valid_digits[digit]
-
-            if last_digit:
-                break
-
-        running_total += int(first_digit + last_digit)
-
+        running_total += int(find_digit(line, 'first') + find_digit(line, 'last'))
 
 print(running_total)
