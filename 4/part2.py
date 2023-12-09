c25834b6c4f70b2d39a2377daf02ac11e0280fe0
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-09 12:55:10 +0000

    Remove unnecessary multiplcation by 1

--- 4/part2.py
+++ 4/part2.py
@@ -18,8 +18,8 @@ with open("input.txt") as f:
         if matching_numbers:
             for i in range(1, len(matching_numbers) + 1):
                 if count_of_cards.get(card_id + i):
-                    count_of_cards[card_id + i] += 1 * count_of_cards[card_id]
+                    count_of_cards[card_id + i] += count_of_cards[card_id]
                 else:
-                    count_of_cards[card_id + i] = 1 * count_of_cards[card_id]
+                    count_of_cards[card_id + i] = count_of_cards[card_id]
 
 print(sum(count_of_cards.values()))
