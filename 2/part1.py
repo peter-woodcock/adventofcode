336ba97b59d3141a24d15537bb48c121104cd09d
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-09 14:03:09 +0000

    Introduce list comprehension to check game validity

--- 2/part1.py
+++ 2/part1.py
@@ -7,27 +7,24 @@ maximum_number_of_cubes = {
 }
 
 
-def given_cubes_greater_than_possible(colour: str):
+def given_cubes_greater_than_possible(colour: str, round) -> bool:
     given_cubes = re.search(f"([0-9]+) {colour}", round)
-    if given_cubes:
-        return int(given_cubes.group(1)) > maximum_number_of_cubes[colour]
+    return int(given_cubes.group(1)) > maximum_number_of_cubes[colour] if given_cubes else False
 
 
 running_total = 0
 
 with open("input.txt", newline="\n") as f:
     for line in f:
-        game_possible = True
-        game_id = int(re.search("([0-9]+):", line).group(1))
-
-        rounds = line[8:].split(";")
-        for round in rounds:
-            for colour in maximum_number_of_cubes.keys():
-                if given_cubes_greater_than_possible(colour):
-                    game_possible = False
-                    break
+        game = line[8:].split(";")
+        game_possible = all(
+            all(not given_cubes_greater_than_possible(colour, round) for colour in maximum_number_of_cubes)
+            for round in game
+        )
 
         if game_possible:
+            game_id = int(re.search("([0-9]+):", line).group(1))
             running_total += game_id
+
 
 print(running_total)
