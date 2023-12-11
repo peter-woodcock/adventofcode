1bfb54cf5aa6321309506fbad808d896352f4f73
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-11 23:48:34 +0000

    Refactor

--- 6/part2.py
+++ 6/part2.py
@@ -1,15 +1,15 @@
 def parse_numeric_values(line: str) -> list:
-    return ''.join([value for value in line.split() if value.isnumeric()])
+    return int(''.join(char for char in line if char.isnumeric()))
 
 
 with open("input.txt") as f:
     contents = f.read().split("\n")
-    time = int(parse_numeric_values(contents[0]))
-    distance = int(parse_numeric_values(contents[1]))
+    time = parse_numeric_values(contents[0])
+    distance = parse_numeric_values(contents[1])
 
     viable_time_count = 0
-    for i in range(1, int(parse_numeric_values(contents[0]))):
-        if i * (int(time) - i) > distance:
+    for i in range(1, time):
+        if i * (time - i) > distance:
             viable_time_count += 1
 
     print(viable_time_count)
