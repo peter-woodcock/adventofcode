c9cf525a77745de5ab63bda00db4b6a6c832ee02
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2023-12-13 19:43:54 +0000

    Refactor: reduce number of lines used

--- 8/part1.py
+++ 8/part1.py
@@ -1,28 +1,24 @@
-with open("input.txt") as f:
+with open("test_input_1.txt") as f:
     contents = f.read().split("\n")
     directions = contents[0]
 
-    node_map_as_list = contents[2:]
-    node_map_as_list = [node.split(' = ') for node in node_map_as_list]
+    node_map_as_list = [node.split(' = ') for node in contents[2:]]
     node_map_as_dict = {node: connected_nodes.strip('()').split(', ') for [node, connected_nodes] in node_map_as_list}
 
-    starting_node = 'AAA'
-    target_node = 'ZZZ'
-
-    current_node = starting_node
     number_of_steps = 0
-    direction_index = 0
+    directions_index = 0
+    current_node = 'AAA'
+    while current_node != 'ZZZ':
+        if directions_index == len(directions):
+            directions_index = 0
 
-    while current_node != target_node:
-        if direction_index == len(directions):
-            direction_index = 0
-        direction = directions[direction_index]
+        direction = directions[directions_index]
         if direction == 'L':
             current_node = node_map_as_dict[current_node][0]
         if direction == 'R':
             current_node = node_map_as_dict[current_node][1]
 
-        direction_index += 1
+        directions_index += 1
         number_of_steps += 1
 
     print(number_of_steps)
